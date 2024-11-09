32b19dd5ff91174da3ac458e115402414aec1d7b
Aidan Hopper <aidanhop1@gmail.com>
2024-11-08 20:12:54 -0800

    starter project

---
+++ main.py
@@ -0,0 +1,30 @@
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 1024
+
+
+
+# Initialize Pygame
+pygame.init()
+
+
+# Set up the display
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # Width: 800, Height: 600
+pygame.display.set_caption("Basic Pygame Window")
+
+
+# Set up colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+        
+if __name__ == '__main__':
+    running = True
+    board = ChessBoard()
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    pygame.quit()
+    sys.exit()
